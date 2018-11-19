c50a94a922eafab83d5bb928c31faf7cefdcb2e9
Francis Addison <faddison@Franciss-MacBook-Pro.local>
2018-11-18 19:03:54 -0500

    initial architecture

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component }  from 'react';
+import '../styles/Header.css'
+
+class Header extends Component {
+    render () {
+        return (
+            <div className="header-container">
+                Hello
+            </div>
+        );
+    }
+}
+
+export default Header;
